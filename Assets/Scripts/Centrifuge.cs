c9c30be391e6cac745020c6d3dc3d5e65ef780f7
Vladislav Yegorov <77292451+sckmat@users.noreply.github.com>
2024-02-14 20:21:39 +0500

    Centrifuge Slot + model

--- Assets/Scripts/Centrifuge.cs
+++ Assets/Scripts/Centrifuge.cs
@@ -1,28 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Centrifuge : MonoBehaviour
 {
-    private TestTube _currentTestTube;
+    [SerializeField] private GameObject centrifugeSlot;
+    private static TestTube _currentTestTube;
+    private static bool _isCentrifugeReady = false;
     private const float CentrifugeDuration = 5.0f;
 
-    private void OnTriggerEnter(Collider other)
+    private void Update()
     {
-        var testTube = other.gameObject.GetComponent<TestTube>();
-        if (testTube == null) return;
-        StartCoroutine(CentrifugeTestTube(testTube));
-        Debug.Log("Start");
-    }
-    
-    private IEnumerator CentrifugeTestTube(TestTube testTube)
-    {
-        yield return new WaitForSeconds(CentrifugeDuration);
-        Debug.Log("Cent");
-        if (testTube != null)
+        if (centrifugeSlot && Input.GetKeyDown(KeyCode.Space))
         {
-            testTube.SetBloodState(TestTube.BloodState.Centrifuged);
+            StartCoroutine(CentrifugeTestTube());
+            Debug.Log("Start Centrifuging");
         }
     }
+    
+    public static void SetTestTube(TestTube testTube)
+    {
+        _currentTestTube = testTube;
+        _isCentrifugeReady = true;
+    }
+    
+    public static void RemoveTestTube()
+    {
+        _currentTestTube = null;
+        _isCentrifugeReady = false;
+    }
+
+    private IEnumerator CentrifugeTestTube()
+    {
+        Debug.Log("Centrifugation started");
+        yield return new WaitForSeconds(CentrifugeDuration);
+        Debug.Log("Centrifugation completed");
+
+        _currentTestTube.SetBloodState(TestTube.BloodState.Centrifuged);
+    }
+    
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     var testTube = other.gameObject.GetComponent<TestTube>();
+    //     if (testTube == null) return;
+    //     StartCoroutine(CentrifugeTestTube(testTube));
+    //     Debug.Log("Start");
+    // }
+    //
+    // private IEnumerator CentrifugeTestTube(TestTube testTube)
+    // {
+    //     yield return new WaitForSeconds(CentrifugeDuration);
+    //     Debug.Log("Cent");
+    //     if (testTube != null)
+    //     {
+    //         testTube.SetBloodState(TestTube.BloodState.Centrifuged);
+    //     }
+    // }
 }
 
---
+++ Assets/Scripts/CentrifugeSlot.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CentrifugeSlot : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("TestTube"))
+        {
+            Debug.Log("OnTriggerEnter");
+            Debug.Log("TRIGGERED: " + other.gameObject.name);
+            Centrifuge.SetTestTube(other.gameObject.GetComponent<TestTube>());
+            other.gameObject.transform.position = transform.position;
+            other.gameObject.transform.rotation = transform.rotation;
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.isKinematic = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("TestTube"))
+        {
+            Debug.Log("OnTriggerExit");
+            Centrifuge.RemoveTestTube();
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            if (rb != null)
+            { 
+                rb.isKinematic = false;
+            }
+        }
+    }
+}
